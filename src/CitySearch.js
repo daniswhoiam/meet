846ab2bde2db2e66e4fba9026839f054a92a5b91
Daniil Belazovschi <daniil.belazovschi@gmail.com>
2021-06-03 19:59:36 +0200

    Integration tests for event filter feature

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -22,6 +22,8 @@ class CitySearch extends Component {
     this.setState({
       query: suggestion
     });
+
+    this.props.updateEvents(suggestion);
   }
 
   render () {
@@ -42,7 +44,10 @@ class CitySearch extends Component {
               {suggestion}
             </li>
           ))}
-          <li key='all'>
+          <li 
+          key='all'
+          onClick={() => this.handleItemClicked("all")}
+          >
             <b>See all cities</b>
           </li>
         </ul>
