69936b10c7ca65fb7f62f35dce36d27d83142503
daniswhoiam <daniil.belazovschi@gmail.com>
2021-06-17 12:02:00 +0200

    Added offline warning

--- src/EventList.js
+++ src/EventList.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Event from './Event';
+import { WarningAlert } from './Alert';
 
 class EventList extends Component {
+  warningMessage = 'You are offline. The displayed information might not be up-to-date';
+
+  state = {
+    warningText: navigator.onLine ? this.warningMessage : ''
+  }
+
+  toggleOfflineMessage = () => {
+    navigator.onLine ? 
+      this.setState({ warningText: this.warningMessage }) :
+      this.setState({ warningText: '' });
+  }
+
   render() {
     const { events } = this.props;
+
+    window.addEventListener('offline', this.toggleOfflineMessage);
+    window.addEventListener('online', this.toggleOfflineMessage);
+
     return(
-      <ul className="EventList">
-        {events.map(event => 
-          <li key={event.id}>
-            <Event event={event} />
-          </li>  
-        )}
-      </ul>
+      <>
+        <WarningAlert warningText={this.state.warningText} />
+        <ul className="EventList">
+          {events.map(event => 
+            <li key={event.id}>
+              <Event event={event} />
+            </li>  
+          )}
+        </ul>
+      </>
     );
   }
 }
